817a1c6b014fb182bfd5c120f1817b207f422fb6
Bashir Gulistani <76609477+BashirGulistani@users.noreply.github.com>
2026-02-10 20:58:37 -0500

    Update utils.py

--- src/quietdiff/utils.py
+++ src/quietdiff/utils.py
@@ -9,6 +9,15 @@ _ws = re.compile(r"\s+")
 
 
 
+def norm_str(x: Any) -> str:
+    if x is None:
+        return ""
+    s = str(x)
+    s = s.strip()
+    s = _ws.sub(" ", s)
+    return s
+
+
 
 
 
