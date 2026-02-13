bfa2149252382ff8c1f1f1bc05aa7f689f4283f8
Bashir Gulistani <76609477+BashirGulistani@users.noreply.github.com>
2026-02-12 23:11:27 -0500

    Create report.py

---
+++ src/quietdiff/report.py
@@ -0,0 +1,12 @@
+from __future__ import annotations
+
+import json
+import os
+from dataclasses import asdict
+from typing import Any
+
+from .diff import DiffResult
+from .utils import norm_str
+
+
+
